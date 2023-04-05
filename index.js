84195a6543999698100fb041652e704998009c83
keyxcode <keyxcode@gmail.com>
2023-04-05 19:24:27 +0700

    Solution 3.3: display info for a single person

--- index.js
+++ index.js
@@ -39,5 +39,13 @@ app.get("/info", (request, response) => {
   );
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((p) => p.id === id);
+
+  if (!person) return response.status(404).end();
+  return response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
