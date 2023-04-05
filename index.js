2fc2f5d8bac8d0584ce17ad665dd6dee5bc87e9c
keyxcode <keyxcode@gmail.com>
2023-04-05 19:59:25 +0700

    Solution 3.5: implement post route to add person

--- index.js
+++ index.js
@@ -60,5 +60,30 @@ app.delete("/api/persons/:id", (request, response) => {
   return response.status(204).end();
 });
 
+const generateRandomId = () => {
+  const RANGE = 100000;
+  return Math.floor(Math.random() * RANGE);
+};
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const person = {
+    ...body,
+    id: generateRandomId(),
+  };
+
+  persons = persons.concat(person);
+  console.log(persons);
+
+  response.json(person);
+});
+
 const PORT = 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
