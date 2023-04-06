95bfbed5332cd20f697ce55ca547de4b6b0093cb
keyxcode <keyxcode@gmail.com>
2023-04-07 02:11:45 +0700

    Solution 3.12: create command line mongo db

--- mongo.js
+++ mongo.js
@@ -1,7 +1,43 @@
 const mongoose = require("mongoose");
 
-if (process.argv.length < 3) {
-  console.log("give password as argument");
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const savePerson = (name, number) => {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+
+  person.save().then((result) => {
+    console.log("person saved!");
+    mongoose.connection.close();
+  });
+};
+
+const fetchAll = () => {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+};
+
+if (process.argv.length === 5) {
+  const name = process.argv[3];
+  const number = process.argv[4];
+  console.log(`saving ${process.argv[3]} - ${process.argv[4]}`);
+  savePerson(name, number);
+} else if (process.argv.length === 3) {
+  fetchAll();
+} else {
+  console.log("password to retrieve all; password name number to save new");
   process.exit(1);
 }
 
@@ -11,30 +47,3 @@ const url = `mongodb+srv://keyxcode:${password}@cluster0.sz9gtfg.mongodb.net/pho
 
 mongoose.set("strictQuery", false);
 mongoose.connect(url);
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: Number,
-});
-
-const Person = mongoose.model("Person", personSchema);
-
-const person = new Person({
-  name: "test",
-  number: 123,
-});
-
-person.save().then((result) => {
-  console.log("person saved!");
-  // close the db connection (necessary)
-  mongoose.connection.close();
-});
-
-// Fetching objects from the database with the find() method
-// If the passed in object is empty => fetch all
-// Note.find({ content: "CSS is hard" }).then((result) => {
-//   result.forEach((note) => {
-//     console.log(note);
-//   });
-//   mongoose.connection.close();
-// });
