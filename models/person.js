1d9278464dfc6c06ab6a6389e60706f8907380d5
keyxcode <keyxcode@gmail.com>
2023-04-08 01:18:06 +0700

    Solution 3.20: add phone number validation

--- models/person.js
+++ models/person.js
@@ -23,7 +23,16 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minLengh: 8,
+    validate: {
+      validator: function (v) {
+        return /\d{2,3}-\d+/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
 });
 
 personSchema.set("toJSON", {
