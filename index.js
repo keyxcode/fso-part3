c64665fd335c521a8ba8a37d5f9564cefbc86ddb
keyxcode <keyxcode@gmail.com>
2023-04-07 20:07:29 +0700

    Solution 3.18: update persons/id and /info route

--- index.js
+++ index.js
@@ -20,26 +20,34 @@ app.get("/api/persons", (request, response, next) => {
     .catch((error) => next(error));
 });
 
-app.get("/info", (request, response) => {
+app.get("/info", (request, response, next) => {
   const dateNow = Date(Date.now());
 
-  Person.find({}).then((persons) => {
-    response.send(
-      `<div>Phonebook has info for ${persons.length} people</div>
+  Person.find({})
+    .then((persons) => {
+      response.send(
+        `<div>Phonebook has info for ${persons.length} people</div>
     <div>${dateNow}</div>`
-    );
-  });
+      );
+    })
+    .catch((error) => next(error));
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id);
-  const person = persons.find((p) => p.id === id);
+app.get("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
 
-  if (!person) return response.status(404).end();
-  return response.json(person);
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then((result) => response.status(204).end())
     .catch((error) => next(error));
