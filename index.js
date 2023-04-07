2b4c15cc26cee0d374e99e8c94ec2a07e0e353f0
keyxcode <keyxcode@gmail.com>
2023-04-08 01:23:10 +0700

    Solution 3.21: ready to deploy

--- index.js
+++ index.js
@@ -56,12 +56,6 @@ app.delete("/api/persons/:id", (request, response, next) => {
 app.post("/api/persons", (request, response, next) => {
   const body = request.body;
 
-  // if (!body.name || !body.number) {
-  //   return response.status(400).json({
-  //     error: "content missing",
-  //   });
-  // }
-
   const person = new Person({
     name: body.name,
     number: body.number,
