17e4016074f63ff8b592585169437e50619c4853
keyxcode <keyxcode@gmail.com>
2023-04-07 19:55:58 +0700

    Solution 3.17: implement put route in the backend to update person given id

--- index.js
+++ index.js
@@ -78,6 +78,19 @@ app.post("/api/persons", (request, response, next) => {
     .catch((error) => next(error));
 });
 
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => response.json(updatedPerson))
+    .catch((err) => next(err));
+});
+
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
 
